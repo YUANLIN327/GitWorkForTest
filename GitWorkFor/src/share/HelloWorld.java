7fd80c86beb5ff0cde05bd7a98725bbc24ad2a50
LIN_YUAN <LIN_YUAN@baylor.edu>
2015-11-05 15:13:13 -0600

    LinYuan1990 Made first change

--- GitWorkFor/src/share/HelloWorld.java
+++ GitWorkFor/src/share/HelloWorld.java
@@ -3,5 +3,8 @@ package share;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hellowa");
+		
+		//Ok, so here I made a change
+		System.out.println("first change");
 	}
 }
