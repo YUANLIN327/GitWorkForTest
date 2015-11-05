1ae9e7500eb08ae5684a7bc5d86d859877253f10
YUANLIN327 <yuanlin.dalian@gmail.com>
2015-11-05 15:29:07 -0600

    A large Change

--- GitWorkFor/src/share/HelloWorld.java
+++ GitWorkFor/src/share/HelloWorld.java
@@ -2,12 +2,7 @@ package share;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hellowa");
-		
-		//Ok, so here I made a change
-		System.out.println("first change");
-		
-		//Ok, so I made a second change
-		System.out.println("second change");
+		//So this is the larger change
+		System.out.println("a large change");
 	}
 }
